dd634372d581352267308ab43c00cfd2e91e6b37
Aparnavjayan <aparnavj95@gmail.com>
2024-08-08 18:54:49 +0530

    error page and subscriptionplan page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,9 @@ import DesignationPage from './DatingFrontend/Pages/Designation/DesignationPage'
 import ViewedMyProfile from './DatingFrontend/Pages/ViewedMyProfile/ViewedMyProfile';
 import MatchesPage from './DatingFrontend/Pages/Matches/MatchesPage';
 import UpgradePopup from './DatingFrontend/Pages/UpgradePopup/UpgradePopup';
+import NotFound404 from './DatingFrontend/Pages/NotFound404/NotFound404';
+import NotFound403 from './DatingFrontend/Pages/NotFound403/NotFound403';
+import SubscriptionPlan from './DatingFrontend/Pages/SubscriptionPlan/SubscriptionPlan';
 
 
 
@@ -22,6 +25,10 @@ function App() {
         <Route path='/viewedwmyprofile' element={<ViewedMyProfile/>} />
         <Route path='/matches' element={<MatchesPage/>} />
         <Route path='/upgradepopup' element={<UpgradePopup/>}/>
+        <Route path='/404' element={<NotFound404/>} />
+        <Route path='/403' element={<NotFound403/>} />
+        <Route path='/subscription' element={<SubscriptionPlan/>} />
+
       </Routes>
     </BrowserRouter>
   )
